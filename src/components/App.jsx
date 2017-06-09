bd04353e01a736ecb74156f1199eacad27ee74e1
Gordana Minovska <ekogoca@gmail.com>
2017-06-08 21:31:10 -0700

    Material-ui theme customization

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
+import {yellow500, darkBlack} from 'material-ui/styles/colors';
 import injectTapEventPlugin from 'react-tap-event-plugin';
 import {
   BrowserRouter as Router,
@@ -14,9 +16,18 @@ import Quizzes from './Quizzes';
 
 injectTapEventPlugin();
 
+const muiTheme = getMuiTheme({
+  palette: {
+    primary1Color: yellow500,
+    textColor: darkBlack,
+  },
+  appBar: { 
+    textColor: darkBlack
+  },
+});
 
 const App = () => (
-  <MuiThemeProvider>
+  <MuiThemeProvider muiTheme={muiTheme}>
     <div>
       <Router>
         <div>
