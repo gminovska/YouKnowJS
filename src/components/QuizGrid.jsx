105e747cfe4131539851940fe1a6e0cf1ee411b6
Kamyl <kamillcybulski@gmail.com>
2017-06-28 01:47:32 +0200

    Basic styling for quizGrid.

--- src/components/QuizGrid.jsx
+++ src/components/QuizGrid.jsx
@@ -1,18 +1,31 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-const QuizGrid = ({quizzes}) => {
+
+const QuizGrid = ({ quizzes}) => {
+
+    const displayInfo = (e) => {
+      e.currentTarget.classList.toggle('displayInfo');
+      console.log(e.currentTarget.classList);
+    }
 
     return (
-        <div>
+        <div className="quizGrid">
+
             {quizzes.map((quiz) => (
-            <div key={quiz._id}>  
-              <h1 >{quiz.name}</h1>
-              <Link to={{
-                pathname: `/quizzes/${quiz._id}`,
-              }}>Link</Link>
+              <div key={quiz._id}
+                   className="quizItem"
+                   onTouchTap={(e) => { displayInfo(e) }} >  
+
+                <h1 >{quiz.name}</h1>
+                <Link to={{ pathname: `/quizzes/${quiz._id}`}}>
+                  Link
+                </Link>
+
             </div>
         ))}
+
+
         </div>
     );
 };
