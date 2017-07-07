ea365b23e16dd5c7e81febb34e02d8fd9405b948
Gordana Minovska <ekogoca@gmail.com>
2017-07-07 16:31:16 -0700

    Create user route

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const path = require('path');
 const config = require('./config');
 const credentials = require('./credentials');
 const mongoose = require('mongoose');
+const passport = require('passport');
+const LocalStrategy = require('passport-local');
 // const MongoClient = require('mongodb').MongoClient;
 const ObjectID = require('mongodb').ObjectID;
 const bodyParser = require('body-parser');
@@ -11,19 +13,42 @@ const app = express();
 
 //models
 const Quiz = require('./models/quiz');
+const User = require('./models/user');
 const populateDB = require('./data');
 
-//setup
+//general setup
 app.use(express.static(path.join(__dirname, 'build')));
 app.use(bodyParser.urlencoded({
     extended: true
 }));
 app.use(bodyParser.json());
 
+//passport configuration
+app.use(require('express-session')({
+    secret: "The earth is flat",
+    resave: false,
+    saveUninitialized: false
+    //TODO include secure cookies in production
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+//passport-local-mongoose setup
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
 //database connection
 mongoose.connect(credentials.url);
 //populate database
 // populateDB();
+
+//make the user object globally available
+app.use(function(req, res, next){
+    res.locals.user = req.user;
+    next();
+});
+
+
 //api routes
 app.get('/api/quizzes', (req, res) => {
 
@@ -68,10 +93,22 @@ app.post('/api/login', (req, res) => {
 })
 
 app.post('/api/signup', (req, res) => {
-  console.log('Somebody wants to sign up!')
-  console.log(req.body.email);
-  res.send("Yoyoyo dawg!")
-})
+   var newUser = new User({
+     username: req.body.email
+   });
+
+   User.register(newUser, req.body.password, (err, newUser) =>{
+      if(err) {
+        console.log(err);
+        res.send(err.message);
+      } 
+      //currently not working...
+      passport.authenticate('local')(req, res, () => {
+            console.log(user);
+            res.redirect('/quizzes');            
+        });
+   });  
+});
 /**
  * Send html file to the client, if nothing else was requested
  */
